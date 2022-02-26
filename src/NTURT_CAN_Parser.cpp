1030cf44637e064717abea03b7e804705a4974ed
Brad <tw88712@gmail.com>
2022-02-27 02:06:29 +0800

    fix macro

--- src/NTURT_CAN_Parser.cpp
+++ src/NTURT_CAN_Parser.cpp
@@ -13,19 +13,19 @@ int Parser::decode(int type, int* data){
         flag_[THR] = 1;
         flag_[BRK] = 1;
         flag_[STR] = 1;
-        return OK_PARSER;
+        return OK;
     }
     else if(type == FB2){
         // TODO
         // No such message yet
-        return OK_PARSER;
+        return OK;
     }
     else if(type == RBX){
         rws_l_ = (data[2]*256+data[3])*0.002;
         rws_r_ = (data[4]*256+data[5])*0.002;
         // flags
         flag_[RWS] = 1;
-        return OK_PARSER;
+        return OK;
     }
     else if(type == HIA){
         accx_ = ((data[3]*256+data[2])-32000)/100;
@@ -33,7 +33,7 @@ int Parser::decode(int type, int* data){
         accz_ = ((data[5]*256+data[4])-32000)/100;
         // flags
         flag_[ACC] = 1;
-        return OK_PARSER;
+        return OK;
     }
     else if(type == HIG){
         gyrx_ = ((data[3]*256+data[2])-32000)/128;
@@ -41,7 +41,7 @@ int Parser::decode(int type, int* data){
         gyrz_ = ((data[5]*256+data[4])-32000)/128;
         // flags
         flag_[GYR] = 1;
-        return OK_PARSER;
+        return OK;
     }
     else if(type == HIC){
         cmpx_ = ((data[2]*65536+data[1]*256+data[0])-8192000)/32768;
@@ -49,22 +49,22 @@ int Parser::decode(int type, int* data){
         // no cmpz_ for Honeywell IMU
         // flags
         flag_[CMP] = 1;
-        return OK_PARSER;
+        return OK;
     }
     // still have to decode BMS and INVerter
     else{
         // Wrong Message
-        return ERR_PARSER;
+        return ERR;
     }
 }
 
 int Parser::encode(int type, double* tbe, int* data){
     if(type==INV){
         // encode torque data here
-        return OK_PARSER;
+        return OK;
     }
     else{
-        return ERR_PARSER;
+        return ERR;
     }
 }
 
@@ -74,11 +74,11 @@ int Parser::get_ACC(double &ax, double &ay, double &az){
         ay = accy_;
         az = accz_;
         flag_[ACC] = 0;
-        return OK_PARSER;
+        return OK;
     }
     else{
         // not decoded yet
-        return ERR_PARSER;
+        return ERR;
     }
 }
 
@@ -88,11 +88,11 @@ int Parser::get_GYR(double &gx, double &gy, double &gz){
         gy = gyry_;
         gz = gyrz_;
         flag_[GYR] = 0;
-        return OK_PARSER;
+        return OK;
     }
     else{
         // not decoded yet
-        return ERR_PARSER;
+        return ERR;
     }
 }
 
@@ -102,11 +102,11 @@ int Parser::get_CMP(double &cx, double &cy, double &cz){
         cy = cmpy_;
         cz = cmpz_;
         flag_[CMP] = 0;
-        return OK_PARSER;
+        return OK;
     }
     else{
         // not decoded yet
-        return ERR_PARSER;
+        return ERR;
     }
 }
 
@@ -115,11 +115,11 @@ int Parser::get_FWS(double &fws_l, double &fws_r){
         fws_l = fws_l_;
         fws_r = fws_r_;
         flag_[FWS] = 0;
-        return OK_PARSER;
+        return OK;
     }
     else{
         // not decoded yet
-        return ERR_PARSER;
+        return ERR;
     }
 }
 
@@ -128,11 +128,11 @@ int Parser::get_RWS(double &rws_l, double &rws_r){
         rws_l = rws_l_;
         rws_r = rws_r_;
         flag_[RWS] = 0;
-        return OK_PARSER;
+        return OK;
     }
     else{
         // not decoded yet
-        return ERR_PARSER;
+        return ERR;
     }
 }
 
@@ -141,11 +141,11 @@ int Parser::get_THR(double &th_1, double &th_2){
         th_1 = thr1_;
         th_2 = thr2_;
         flag_[THR] = 0;
-        return OK_PARSER;
+        return OK;
     }
     else{
         // not decoded yet
-        return ERR_PARSER;
+        return ERR;
     }
 }
     
@@ -153,11 +153,11 @@ int Parser::get_BRK(double &brk){
     if(flag_[BRK]){
         brk = brk_;
         flag_[BRK] = 0;
-        return OK_PARSER;
+        return OK;
     }
     else{
         // not decoded yet
-        return ERR_PARSER;
+        return ERR;
     }
 }
 
@@ -165,11 +165,11 @@ int Parser::get_STR(double &str){
     if(flag_[STR]){
         str = str_;
         flag_[STR] = 0;
-        return OK_PARSER;
+        return OK;
     }
     else{
         // not decoded yet
-        return ERR_PARSER;
+        return ERR;
     }
 
 }
