3e73ecdb8cb4aa0e7b093921d2f415fe5998e417
Brad_Bo-Ruei <bradbr.huang@farobottech.com>
2022-03-02 16:19:03 +0800

    Added openIMU addresses

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nturt_can_parser
 A package that enable a program to parse and encode CAN message defined on NTURT race car.
+- [HackMD Doc Here](https://hackmd.io/4qtuOHkrRgCJhWhzTcpmWA?view)
--- inc/NTURT_CAN_Parser.hpp
+++ inc/NTURT_CAN_Parser.hpp
@@ -11,8 +11,8 @@
 
 #endif
 // define some parameters
-#define Frame_NUM 9 // how many kinds of frame here? plus 1 then.
-#define DATA_NUM 11 // how many kinds of data here? plus 1 then.
+#define Frame_NUM 11 // how many kinds of frame here?
+#define DATA_NUM 10  // how many kinds of data here?
 // define Frame type here
 #define _CP_FB1 1  // front box
 #define _CP_FB2 2  // front box
@@ -22,6 +22,9 @@
 #define _CP_HIA 6  // accelerometer frame from Honeywell IMU
 #define _CP_HIG 7  // gyroscope frame from Honeywell IMU
 #define _CP_HIC 8  // pose frame from Honeywell IMU
+#define _CP_OIA 9  // accelerometer frame from Open IMU
+#define _CP_OIG 10 // gyroscope frame from Open IMU
+#define _CP_OIC 11 // pose frame from Open IMU
 // and so on...
 // define data type here
 #define _CP_ACC 1  // accelerometer
@@ -29,188 +32,209 @@
 #define _CP_CMP 3  // compass
 #define _CP_FWS 4  // front wheel speed
 #define _CP_RWS 5  // rear wheel speed
-#define _CP_THR 6  // throttle 
+#define _CP_THR 6  // throttle
 #define _CP_BRK 7  // brake
 #define _CP_STR 8  // steer
 #define _CP_BTP 9  // battery temp
 #define _CP_BVT 10 // battery voltage
 // and so on...
+// define Constants
+#define TWOPOW08 256
+#define TWOPOW26 67108864
 
 class NTURT_CAN_parser {
 public:
-    /* parameter:
-     *   Frame type
-     *   CAN id
-     * return:
-     *   return OK or ERR
-     */
-    int assign_id(int type, int id){
-        int idsize = sizeof(id_)/sizeof(int); // Should be Frame_NUM
-        if(type<idsize){
-            id_[type] = id;
-            return OK;
-        }
-        else{
-            return ERR;
-        }
+  /* parameter:
+   *   Frame type
+   *   CAN id (or PGN of the id)
+   * return:
+   *   return OK or ERR
+   */
+  int assign_id(int type, int id) {
+    int idsize = sizeof(id_) / sizeof(int); // Should be Frame_NUM
+    if (type < idsize) {
+      id_[type] = id;
+      return OK;
+    } else {
+      return ERR;
     }
-    
-    /* parameter:
-     * return:
-     *   return OK or ERR
-     */
-    int print_id(){
-        for(int i=1;i<Frame_NUM;i++){
-            std::cout<<"type: "<<i<<", id: "<<id_[i]<<std::endl;
-        }
-        return OK;
+  }
+
+  /* parameter:
+   * return:
+   *   return OK or ERR
+   */
+  int print_id() {
+    for (int i = 1; i < Frame_NUM; i++) {
+      std::cout << "type: " << i << ", id: " << id_[i] << std::endl;
     }
+    return OK;
+  }
 
-    /* parameter:
-     *   none
-     * return:
-     *   OK or ERR
-     * assign all ID
-     */
-    int init_parser();
+  /* parameter:
+   * return:
+   *   return OK or ERR
+   * Get PGN for a CAN ID
+   * Check here for PGN meanings:
+   * https://www.kvaser.com/about-can/higher-layer-protocols/j1939-introduction/
+   */
+  int get_PGN(int id) {
+    // we want the
+    id /= TWOPOW08;
+    id = id % TWOPOW26;
+    return id;
+  }
 
-    /* parameter:
-     *   CAN id
-     * return:
-     *   Frame type
-     */
-    int get_type(int id){
-        for(int i=1;i<=Frame_NUM;i++){
-            if(id_[i] == id){
-                return i;
-            }
-        }
-        return ERR;
+  /* parameter:
+   *   none
+   * return:
+   *   OK or ERR
+   * assign all ID
+   */
+  int init_parser();
+
+  /* parameter:
+   *   CAN id (or PGN of the id)
+   * return:
+   *   Frame type
+   */
+  int get_type(int id) {
+    // Check with full ID
+    for (int i = 1; i <= Frame_NUM; i++) {
+      if (id_[i] == id) {
+        return i;
+      }
     }
-    
-    /* parameter:
-     *   Frame type
-     * return:
-     *   CAN id
-     */
-    int get_id(int type){
-        return id_[type];
+    // check with PGN
+    for (int i = 1; i <= Frame_NUM; i++) {
+      if (id_[i] == get_PGN(id)) {
+        return i;
+      }
     }
+    return ERR;
+  }
 
-    /* parameters:
-     *   Frame type
-     *   CAN data (uint[8])
-     * return:
-     *   OK or ERR
-     * It decode data according to Frame type,
-     * and it store the result to the class member variable.
-     */
-    int decode(int type, int* data);
-    
-    /* parameters:
-     *   Frame type
-     *   data to-be-encoded (double [NUM])
-     *   CAN data (uint[8])
-     * return:
-     *   CAN id
-     * It encode tbe (data to-be-encoded) according to Frame type,
-     * and it store the result to data (int[8]).
-     */
-    int encode(int type, double* tbe, int* data);
-    
-    /* parameters:
-     *   variables to-be-updated(i.e. sensor data)
-     *   Here they are accelerometer data
-     * return:
-     *   OK or ERR
-     */
-    int get_ACC(double &ax, double &ay, double &az);
-    
-    /* parameters:
-     *   variables to-be-updated(i.e. sensor data)
-     *   Here they are gyroscope data
-     * return:
-     *   OK or ERR
-     */
-    int get_GYR(double &gx, double &gy, double &gz);
+  /* parameter:
+   *   Frame type
+   * return:
+   *   CAN id (or PGN of the id)
+   */
+  int get_id(int type) { return id_[type]; }
 
-    /* parameters:
-     *   variables to-be-updated(i.e. sensor data)
-     *   Here they are pose (compass) data (roll pitch yaw)
-     * return:
-     *   OK or ERR
-     */
-    int get_CMP(double &cx, double &cy, double &cz);
+  /* parameters:
+   *   Frame type
+   *   CAN data (uint[8])
+   * return:
+   *   OK or ERR
+   * It decode data according to Frame type,
+   * and it store the result to the class member variable.
+   */
+  int decode(int type, int *data);
 
-    /* parameters:
-     *   variables to-be-updated(i.e. sensor data)
-     *   Here they are front wheel speed data (left/right)
-     * return:
-     *   OK or ERR
-     */
-    int get_FWS(double &fws_l, double &fws_r);
-    
-    /* parameters:
-     *   variables to-be-updated(i.e. sensor data)
-     *   Here they are rear wheel speed data (left/right)
-     * return:
-     *   OK or ERR
-     */
-    int get_RWS(double &rws_l, double &rws_r);
-    
-    /* parameters:
-     *   variables to-be-updated(i.e. sensor data)
-     *   Here they are throttle data
-     * return:
-     *   OK or ERR
-     */
-    int get_THR(double &th_1, double &th_2);
-    
-    /* parameters:
-     *   variables to-be-updated(i.e. sensor data)
-     *   Here they are brake data
-     * return:
-     *   OK or ERR
-     */
-    int get_BRK(double &brk);
-    
-    /* parameters:
-     *   variables to-be-updated(i.e. sensor data)
-     *   Here they are steer data
-     * return:
-     *   OK or ERR
-     */
-    int get_STR(double &str);
-    // and BMS data... 
+  /* parameters:
+   *   Frame type
+   *   data to-be-encoded (double [NUM])
+   *   CAN data (uint[8])
+   * return:
+   *   CAN id
+   * It encode tbe (data to-be-encoded) according to Frame type,
+   * and it store the result to data (int[8]).
+   */
+  int encode(int type, double *tbe, int *data);
+
+  /* parameters:
+   *   variables to-be-updated(i.e. sensor data)
+   *   Here they are accelerometer data
+   * return:
+   *   OK or ERR
+   */
+  int get_ACC(double &ax, double &ay, double &az);
+
+  /* parameters:
+   *   variables to-be-updated(i.e. sensor data)
+   *   Here they are gyroscope data
+   * return:
+   *   OK or ERR
+   */
+  int get_GYR(double &gx, double &gy, double &gz);
+
+  /* parameters:
+   *   variables to-be-updated(i.e. sensor data)
+   *   Here they are pose (compass) data (roll pitch yaw)
+   * return:
+   *   OK or ERR
+   */
+  int get_CMP(double &cx, double &cy, double &cz);
+
+  /* parameters:
+   *   variables to-be-updated(i.e. sensor data)
+   *   Here they are front wheel speed data (left/right)
+   * return:
+   *   OK or ERR
+   */
+  int get_FWS(double &fws_l, double &fws_r);
+
+  /* parameters:
+   *   variables to-be-updated(i.e. sensor data)
+   *   Here they are rear wheel speed data (left/right)
+   * return:
+   *   OK or ERR
+   */
+  int get_RWS(double &rws_l, double &rws_r);
+
+  /* parameters:
+   *   variables to-be-updated(i.e. sensor data)
+   *   Here they are throttle data
+   * return:
+   *   OK or ERR
+   */
+  int get_THR(double &th_1, double &th_2);
+
+  /* parameters:
+   *   variables to-be-updated(i.e. sensor data)
+   *   Here they are brake data
+   * return:
+   *   OK or ERR
+   */
+  int get_BRK(double &brk);
+
+  /* parameters:
+   *   variables to-be-updated(i.e. sensor data)
+   *   Here they are steer data
+   * return:
+   *   OK or ERR
+   */
+  int get_STR(double &str);
+  // and BMS data...
 private:
-    /* The id of different CAN message frame.
-     * ex: id[FBX] is the id of front box messages
-     */
-    int id_[Frame_NUM];
-    
-    /* The flag of different sensor data, which indicates if there is new data.
-     * ex: flag[ACC] means if we got new accelerometer data.
-     * If we got message from a node,
-     * then flag[data from that node] will be 1.
-     * If we accessed this data via get_XXX(),
-     * then flag[the data] will be 0
-     */
-    int flag_[DATA_NUM];
-    
-    // declare every data as member function
-    // IMU
-    double accx_, accy_, accz_, gyrx_, gyry_, gyrz_, cmpx_, cmpy_, cmpz_;
-    // Front box
-    double brk_, thr1_, thr2_, str_;
-    // Wheel Speed
-    double fws_l_, fws_r_, rws_l_, rws_r_;
-    // Suspension ride height
-    double frh_l_, frh_r_, rrh_l_, rrh_r_;
-    // Rear box
-    // BMS
-    // Inverter
-    // Dashboard
-    // and so on.......
+  /* The id of different CAN message frame.
+   * ex: id[FBX] is the id of front box messages
+   */
+  int id_[Frame_NUM + 1];
+
+  /* The flag of different sensor data, which indicates if there is new data.
+   * ex: flag[ACC] means if we got new accelerometer data.
+   * If we got message from a node,
+   * then flag[data from that node] will be 1.
+   * If we accessed this data via get_XXX(),
+   * then flag[the data] will be 0
+   */
+  int flag_[DATA_NUM + 1];
+
+  // declare every data as member function
+  // IMU
+  double accx_, accy_, accz_, gyrx_, gyry_, gyrz_, cmpx_, cmpy_, cmpz_;
+  // Front box
+  double brk_, thr1_, thr2_, str_;
+  // Wheel Speed
+  double fws_l_, fws_r_, rws_l_, rws_r_;
+  // Suspension ride height
+  double frh_l_, frh_r_, rrh_l_, rrh_r_;
+  // Rear box
+  // BMS
+  // Inverter
+  // Dashboard
+  // and so on.......
 };
 
 typedef NTURT_CAN_parser Parser;
