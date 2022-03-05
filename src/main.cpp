9014023c2939a8c792eda177ecf8f21e16e575f8
yuhsinchan <b08901046@g.ntu.edu.tw>
2022-03-05 16:58:32 +0000

    modify encode

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <NTURT_CAN_Parser.hpp>
 #include <iostream>
 
-// This program is to 
+// This program is to
 // - Validate the CAN Parser
 // - Demostrate the usage
-int main(){
-    // For example, we want to recognize 
+int main() {
+    // For example, we want to recognize
     // "front box frame 1" and collect data
     Parser myparser;
     myparser.assign_id(_CP_FB1, 0x040AD091);
     // Say, we are in some callbacks and got a
-    // can message.  Then we store the can id 
+    // can message.  Then we store the can id
     // and data into our variables.
     int id = 0x040AD091;
     int data[8];
@@ -26,19 +26,19 @@ int main(){
     // example, front wheel speed
     double fwsr, fwsl;
     int tmptype = myparser.get_type(id);
-    if(tmptype == _CP_FB1){
-        if(myparser.decode(_CP_FB1, data)==-1){
+    if (tmptype == _CP_FB1) {
+        if (myparser.decode(_CP_FB1, data) == -1) {
             myparser.get_FWS(fwsl, fwsr);
         }
     }
-    std::cout<<"Front wheel speed left: "<<fwsl
-            <<", right: "<<fwsr<<std::endl;
+    std::cout << "Front wheel speed left: " << fwsl << ", right: " << fwsr
+              << std::endl;
     // Another example is that we may want to encode torque data
     // which we are going to send it to Inverter.
     double tbe[1];
     double torque = 0;
     tbe[0] = torque;
-    myparser.encode(_CP_INV, tbe, data);
+    // myparser.encode(_CP_INV, tbe, data);
     // Then we can send the "data" to CAN
     return 0;
 }
